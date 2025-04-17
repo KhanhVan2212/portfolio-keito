ee05898ce77f90e0b991e494bc5f6a6d0cb81f10
Nguyễn Khánh Văn <110532548+KhanhVan2212@users.noreply.github.com>
2025-04-17 11:21:12 +0700

    Update contact-email.tsx

--- components/contact-email.tsx
+++ components/contact-email.tsx
@@ -1,6 +1,6 @@
 export default function ContactEmail() {
   return (
-    <a href="mailto:aakash4dev.me@gmail.com" className="text-purple-400 hover:text-purple-300 mt-2 inline-block">
+    <a href="mailto:nguyenkhanhvan221202@gmail.com" className="text-purple-400 hover:text-purple-300 mt-2 inline-block">
       nguyenkhanhvan221202@gmail.com
     </a>
   )
